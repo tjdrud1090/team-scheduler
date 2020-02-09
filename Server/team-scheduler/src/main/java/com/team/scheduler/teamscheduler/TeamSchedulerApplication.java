b279c516e273c8c4270d642fa8673124685bfc80
tjdrud1090 <psk4564@naver.com>
2020-02-09 18:02:50 +0900

    init project backend

---
+++ Server/team-scheduler/src/main/java/com/team/scheduler/teamscheduler/TeamSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.team.scheduler.teamscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSchedulerApplication.class, args);
+    }
+
+}
