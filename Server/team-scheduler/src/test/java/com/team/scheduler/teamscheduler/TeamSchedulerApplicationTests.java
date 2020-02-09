2dfdcf3c754e67ea246270f471b906e7b48eda79
tjdrud1090 <psk4564@naver.com>
2020-02-09 18:02:50 +0900

    init project backend

---
+++ Server/team-scheduler/src/test/java/com/team/scheduler/teamscheduler/TeamSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.scheduler.teamscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
