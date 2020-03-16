be0356f7ad55b53d076bf8ad52f4d450bc5b98a9
tjdrud1090 <psk4564@naver.com>
2020-03-16 23:34:02 +0900

    test commit logback 설정

--- Server/team-scheduler/src/main/java/com/team/scheduler/teamscheduler/TeamSchedulerApplication.java
+++ Server/team-scheduler/src/main/java/com/team/scheduler/teamscheduler/TeamSchedulerApplication.java
@@ -1,13 +1,26 @@
 package com.team.scheduler.teamscheduler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.validation.constraints.Email;
+
 @SpringBootApplication
+@Slf4j
 public class TeamSchedulerApplication {
+
+    @Email(regexp = ".+@.+\\\\..+")
+    String emailAddress = "name@me_ed.com";
 
     public static void main(String[] args) {
         SpringApplication.run(TeamSchedulerApplication.class, args);
+
+        System.out.println("Start");
+        log.info("[{}] - complete", "testtesttestest");
+
+
+        //PR 요청
     }
 
 }
